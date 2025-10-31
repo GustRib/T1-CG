5c1f620e1e36a59d49b65bf5c298454981a2dd37
ssurerus <saulosurerus@hotmail.com>
2025-10-30 22:24:08 -0300

    Inicializando Track 1

--- T1/main.js
+++ T1/main.js
@@ -19,22 +19,59 @@ light = initDefaultBasicLight(scene);
 scene.add(camera);
 
 
-//orbit = new OrbitControls(camera, renderer.domElement);
+orbit = new OrbitControls(camera, renderer.domElement);
 
 
 window.addEventListener('resize', () => onWindowResize(camera, renderer), false);
 window.addEventListener('keydown', (e) => keys[e.key.toLowerCase()] = true);
 window.addEventListener('keyup', (e) => keys[e.key.toLowerCase()] = false);
 
-let plane = createGroundPlaneXZ(60, 60);
-plane.material = setDefaultMaterial("#555555");
+let plane = createGroundPlaneXZ(200, 200);
+plane.material = setDefaultMaterial("#5b9452");
 plane.position.set(0, 0, 0);
 scene.add(plane);
 
+let startLineGeometry = new THREE.PlaneGeometry(10, 10);
+let startLineMaterial = setDefaultMaterial("orange");
+let startLine = new THREE.Mesh(startLineGeometry, startLineMaterial);
+
+function createrTrack1(){
+  let floorWidth = 10;
+  let floorHeight = 10;
+
+  let group = new THREE.Group();  
+  for (let i = 0; i < 10; i++) {
+      let floor = {};
+      const floorGeometry = new THREE.PlaneGeometry(floorWidth, floorHeight);
+      const floorMaterial = setDefaultMaterial("#555555");
+      
+      
+      
+      if(i == 3){
+        floor['left'] = startLine;
+      }
+
+      const divisor = new THREE.PlaneGeometry(floorWidth, 0.1);
+      const matDivisor = setDefaultMaterial("red");
+      const divisorMesh = new THREE.Mesh(divisor, matDivisor);
+      divisorMesh.position.set(0, 4.95, 0.1); // ligeiramente acima do chão
+      floor.add(divisorMesh);
+
+      floor.rotation.x = -Math.PI / 2; // de pé pra deitado
+      floor.position.set(-40, 0, 50 - (i*floorWidth)); // ✅ agora funciona
+      group.add(floor);
+  }
+  
+  return group;
+}
+let grid = new THREE.GridHelper(200, 20);
+scene.add(grid);
+scene.add(createrTrack1());
 
 function render() {
   requestAnimationFrame(render);
   renderer.render(scene, camera);
+ 
 }
 
 render();
